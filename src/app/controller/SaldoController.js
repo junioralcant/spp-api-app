466467e44c8c765330014b908ff56f25a9a9a9ce
junioralcant <junioralcant111@gmail.com>
2022-06-01 17:22:19 -0300

    Add register saldo and list all

--- src/app/controller/SaldoController.js
+++ src/app/controller/SaldoController.js
@@ -1,10 +1,50 @@
+const moment = require('moment');
 const Saldo = require('../models/Saldo');
 
 class SaldoController {
   async index(req, res) {
-    const saldo = await Saldo.find();
+    const { dataIncio, dataFim, allDatas } = req.query;
 
-    return res.json(saldo[0]);
+    console.log(allDatas);
+
+    const filters = {};
+
+    let saldo = await Saldo.find().sort('-createdAt');
+
+    //Filtro de data e nome linha
+    if (dataIncio && dataFim) {
+      filters.createdAt = {};
+
+      const inicio = moment(dataIncio).format(
+        'YYYY-MM-DDT00:mm:ss.SSSZ'
+      );
+
+      const fim = moment(dataFim).format('YYYY-MM-DDT23:59:ss.SSSZ');
+
+      filters.createdAt.$gte = inicio;
+      filters.createdAt.$lte = fim;
+
+      let saldoFilter = await Saldo.paginate(filters, {
+        page: req.query.page || 1,
+        limit: parseInt(req.query.limit_page) || 1000000,
+        sort: '-createdAt',
+      });
+
+      saldo = saldoFilter.docs;
+    }
+
+    // Filtra por dados do mes e ano atual
+    if ((!dataIncio || !dataFim) && !allDatas) {
+      saldo = saldo.filter(
+        (aliment) =>
+          moment(aliment.createdAt).month() ===
+            moment(Date.now()).month() &&
+          moment(aliment.createdAt).year() ===
+            moment(Date.now()).year()
+      );
+    }
+
+    return res.json(saldo);
   }
   async store(req, resp) {
     const saldo = await Saldo.create(req.body);
@@ -19,6 +59,14 @@ class SaldoController {
     });
 
     return res.json(saldo);
+  }
+
+  async delete(req, res) {
+    const saldo = await Saldo.findById(req.params.id);
+
+    await saldo.remove();
+
+    return res.send();
   }
 }
 
--- src/routes/Saldo.js
+++ src/routes/Saldo.js
@@ -6,5 +6,6 @@ const saldo = express.Router();
 saldo.get('/saldo', SaldoController.index);
 saldo.post('/saldo', SaldoController.store);
 saldo.put('/saldo/:id', SaldoController.update);
+saldo.delete('/saldo/:id', SaldoController.delete);
 
 module.exports = saldo;
